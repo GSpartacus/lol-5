82db090701abd30c8b36a6c2b347e11582f94a25
iRaxe <djvinx@live.com>
2015-11-29 01:33:30 +0100

    Update MyActivator.cs

--- KzKarthus/MyActivator.cs
+++ KzKarthus/MyActivator.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using EloBuddy;
 using EloBuddy.SDK;
 
@@ -6,16 +7,11 @@ namespace KzKarthus
     class MyActivator
     {
         public static Spell.Targeted ignite;
-        public static Item talisman, randuin, mikael, ironsolari, fotmountain, glory, youmus, botrk, bilgewater;
+        public static Item Zhonya;
         public static Spell.Targeted smite;
-        public static Spell.Active heal;
+        public static Spell.Active heal, barrier;
         public static void loadSpells()
         {
-            if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("smite"))
-                smite = new Spell.Targeted(SpellSlot.Summoner1, 570);
-            else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("smite"))
-                smite = new Spell.Targeted(SpellSlot.Summoner2, 570);
-
             if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("dot"))
                 ignite = new Spell.Targeted(SpellSlot.Summoner1, 580);
             else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("dot"))
@@ -25,15 +21,12 @@ namespace KzKarthus
                 heal = new Spell.Active(SpellSlot.Summoner1);
             else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("heal"))
                 heal = new Spell.Active(SpellSlot.Summoner2);
-            talisman = new Item((int)ItemId.Talisman_of_Ascension);
-            randuin = new Item((int)ItemId.Randuins_Omen);
-            glory = new Item((int)ItemId.Righteous_Glory);
-            fotmountain = new Item((int)ItemId.Face_of_the_Mountain);
-            mikael = new Item((int)ItemId.Mikaels_Crucible);
-            ironsolari = new Item((int)ItemId.Locket_of_the_Iron_Solari);
-            youmus = new Item((int)ItemId.Youmuus_Ghostblade);
-            botrk = new Item((int)ItemId.Blade_of_the_Ruined_King);
-            bilgewater = new Item((int)ItemId.Bilgewater_Cutlass);
+
+            if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("barrier"))
+                barrier = new Spell.Active(SpellSlot.Summoner1);
+            else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("barrier"))
+                barrier = new Spell.Active(SpellSlot.Summoner2);
+            Zhonya = new Item((int)ItemId.Zhonyas_Hourglass);
         }
     }
 }
